4b04d7b4aaf37dacb3be85320cb239d371425b7e
Stefko1998 <Simjanovski998@gmail.com>
2022-11-25 15:18:02 +0100

    basics-working with components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
